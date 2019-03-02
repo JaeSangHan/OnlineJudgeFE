af758d6e349c3d532ed70dd9f60fe1273b597bbd
Sensol <41419137+JaeSangHan@users.noreply.github.com>
2019-03-02 22:27:55 +0900

    Update App.vue

--- src/pages/oj/App.vue
+++ src/pages/oj/App.vue
@@ -7,7 +7,7 @@
       </transition>
       <div class="footer">
         <p v-html="website.website_footer"></p>
-        <p>Powered by <a href="https://github.com/QingdaoU/OnlineJudge">OnlineJudge</a>
+        <p>Powered by <a href="https://jaesanghan.github.io/My-portfolio/">ÇÑÀç»ó</a>
           <span v-if="version">&nbsp; Version: {{ version }}</span>
         </p>
       </div>
