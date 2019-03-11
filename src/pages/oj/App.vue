e9a389ad8e20a7529d7b6c80b3d6beffd3393c9e
Sensol <41419137+JaeSangHan@users.noreply.github.com>
2019-03-11 17:36:09 +0900

    코이동아리로 이름 바꿈
    설명

--- src/pages/oj/App.vue
+++ src/pages/oj/App.vue
@@ -7,7 +7,7 @@
       </transition>
       <div class="footer">
         <p v-html="website.website_footer"></p>
-        <p>Powered by <a href="https://jaesanghan.github.io/My-portfolio/">한재상</a>
+        <p>Powered by <a href="https://jaesanghan.github.io/My-portfolio/">코이 동아리</a>
           <span v-if="version">&nbsp; Version: {{ version }}</span>
         </p>
       </div>
